45406306b5394ea87b5a73a593abc2987d2891ad
Daniil Murashov <DaniilMurashov@icloud.com>
2025-08-01 23:40:08 -0500

    Implemented a server main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include "battery.hpp"
 #include <iostream>
+#include <httplib.h>
 
 using namespace std;
 
-int main() {
-    cout << BatteryAPI::getBatteryPercentage() << endl;
-    return 0;
+int main()
+{
+    httplib::Server svr;
+
+    svr.Get("/battery", [](const httplib::Request&, httplib::Response& res)
+    {
+        try
+        { 
+            int percent = BatteryAPI::getBatteryPercentage();
+            res.set_content(to_string(percent), "text/plain");
+        }
+        catch (const std::exception& e)
+        {
+            res.status = 500;
+            res.set_content(e.what(), "text/plain");
+        }
+    });
+
+    cout << "Server started at http:://localhost:8080\n";
+    svr.listen("0.0.0.0", 8080);
 }
