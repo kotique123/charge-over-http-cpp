317b95d0ff903fcc02d4177b56540896684f60aa
Daniil Murashov <d.murashov@icloud.com>
2025-08-06 00:10:04 -0500

    Edited main and test files for a new feature

--- tests/test-json-creation.cpp
+++ tests/test-json-creation.cpp
@@ -6,7 +6,7 @@ auto charge = BatteryAPI::getBatteryPercentage();
 
 int main() {
     auto charge = BatteryAPI::getBatteryPercentage();
-    json battery_info = getBatteryCharge(charge);
+    json battery_info = getBatteryData(charge);
     std::cout << battery_info;
     return 0;
 }
