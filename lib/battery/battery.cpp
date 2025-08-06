120cdedb9a044e92a9c9bfeb6f3b70f49e64efaa
Daniil Murashov <d.murashov@icloud.com>
2025-08-05 23:22:25 -0500

    Added multiple implemnetations of int getBatteryCharge() for different platforms

--- lib/battery/battery.cpp
+++ lib/battery/battery.cpp
@@ -3,8 +3,9 @@
 #include <stdexcept>
 
 using namespace std;
-
-int BatteryAPI::getBatteryPercentage() 
+namespace BatteryAPI {
+#if defined(__linux__)
+int getBatteryPercentage() 
 {
     ifstream file("/sys/class/power_supply/BAT0/capacity");
 
@@ -15,4 +16,31 @@ int BatteryAPI::getBatteryPercentage()
     int percentage;
     file >> percentage;
     return percentage;
+}
+#elif(__APPLE__)
+#include <IOkit/ps/IOPowerSources.h>
+#include <IOKit/ps/IOPSKeys.h>
+#include <CoreFoundation/CoreFoundation.h>
+int getBatteryPercentage() {
+    CFTypeRef blob = IOPSCopyPowerSourcesInfo();
+    CFArrayRef sources = IOPSCopyPowerSourcesList(blob);
+    if (CFArrayGetCount(sources) == 0) return -1;
+
+    CFDictionaryRef pSource = IOPSGetPowerSourceDescription(blob, CFArrayGetValueAtIndex(sources, 0));
+    if (!pSource) return -1;
+
+    CFNumberRef capacity = (CFNumberRef)CFDictionaryGetValue(pSource, CFSTR(kIOPSCurrentCapacityKey));
+    int percent = -1;
+    CFNumberGetValue(capacity, kCFNumberIntType, &percent);
+    return percent;
+}
+#elif(_WIN32)
+int getBatteryPercentage() {
+    SYSTEM_POWER_STATUS sps;
+    if (GetSystemPowerStatus(&sps)) {
+        return sps.BatteryLifePercent; // 0–100
+    }
+    return -1;
+}
+#endif
 }
